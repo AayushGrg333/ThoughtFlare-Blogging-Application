78851a0b7b92c0a870b3c99d673af6dcad050a9e
AayushGrg333 <aayushgrg333@gmail.com>
2024-09-24 10:45:13 +0545

    started doing some setup for the application

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require("express")
+const path = require("path")
+
+const app = express();
+const PORT  = process.env.PORT;
+
+//setting view engine
+app.set("view engine",'ejs');
+app.set("views",path.resolve("./views"));
+
+//setting path
+app.get('/', (req,res)=>{
+    res.render('home');
+})
+
+//starting server
+app.listen(PORT, ()=> console.log(`Server has been started Port:${PORT}`))
